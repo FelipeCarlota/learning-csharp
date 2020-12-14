02af28151d808ee27658dacb70720ed6cd4ddfcc
FelipeCarlota <felipe.carlota@gmail.com>
2020-12-14 17:29:37 -0400

    Operações com objeto dinâmico

--- novo-projeto/aula2/6 - Dynamic/depois/Dinamicos.cs
+++ novo-projeto/aula2/6 - Dynamic/depois/Dinamicos.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace certificacao_csharp_roteiro
 {
-    class Dinamicos : IAulaItem
+  class Dinamicos : IAulaItem
+  {
+    public void Executar()
     {
-        public void Executar()
-        {
-
-        }
+      object objeto = 1;
+      //objeto = objeto + 3; //O programa não permite fazer esta operação, pois não se pode
+      // realizar operação de um objeto com variáveis de outro tipo.
+      dynamic dinamico = 1;
+      dinamico = dinamico + 3;
+      Console.WriteLine($"O valor de dinâmico, que permite realizar operações como sendo objeto com outros tipos é: {dinamico}");
     }
+  }
 }
