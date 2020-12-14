bff07b85373cc4c5510795e78b25b642f4201676
FelipeCarlota <felipe.carlota@gmail.com>
2020-12-13 22:27:33 -0400

    Blooleanos

--- novo-projeto/aula1/5 - Booleanos/depois/Booleanos.cs
+++ novo-projeto/aula1/5 - Booleanos/depois/Booleanos.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace certificacao_csharp_roteiro
 {
-    class Booleanos : IAulaItem
+  class Booleanos : IAulaItem
+  {
+    public void Executar()
     {
-        public void Executar()
-        {
-        }
+      //bool possuiSaldo = 1;
+      bool possuiSaldo = true;
+
+      Console.WriteLine($"possuiSaldo: {possuiSaldo}");
+
+      //trecho do código omitido
+
+      int dias = DateTime.Now.Day;
+      Console.WriteLine($"dias: {dias}");
+
+      //Atribui a diasPar o valor de uma expressão booleana
+      bool diasPar = (dias % 2 == 0);
+
+      if (diasPar == true)
+      {
+        Console.WriteLine("dias é um número par");
+      }
+      else
+      {
+        Console.WriteLine("dias é um número ímpar");
+      }
+
+      //   //Atribui a diasPar o valor de uma expressão booleana
+      //   bool diasPar = (dias % 2 == 0);
+
+      //   if diasPar
+      //   {
+      //     Console.WriteLine("dias é um número par");
+      //   }
+      //   else
+      //   {
+      //     Console.WriteLine("dias é um número ímpar");
+      //   }
+
     }
+  }
 }
