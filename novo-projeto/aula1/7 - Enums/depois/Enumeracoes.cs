263b68c9ba9f8431fb3f657f2e5cb5fae1a6dcdc
FelipeCarlota <felipe.carlota@gmail.com>
2020-12-14 00:08:13 -0400

    Atualização sobre struct e enum

--- novo-projeto/aula1/7 - Enums/depois/Enumeracoes.cs
+++ novo-projeto/aula1/7 - Enums/depois/Enumeracoes.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace certificacao_csharp_roteiro
 {
-    class Enumeracoes : IAulaItem
+  class Enumeracoes : IAulaItem
+  {
+    public void Executar()
     {
-        public void Executar()
-        {
-        }
+      DiasDaSemana primeiroDia = DiasDaSemana.Seg;
+
+      DiasDeTrabalho diasDeTrabalho
+          = DiasDeTrabalho.Ter | DiasDeTrabalho.Qui | DiasDeTrabalho.Sex;
+
+      Console.WriteLine($"O primeiro dia da semana é para se trabalhar é: {primeiroDia}");
+      Console.WriteLine(diasDeTrabalho);
     }
+  }
+
+  enum DiasDaSemana : long { Seg, Ter, Qua, Qui, Sex, Sab, Dom }
+  [Flags]
+  enum DiasDeTrabalho { Seg = 0, Ter = 1, Qua = 2, Qui = 4, Sex = 8, Sab = 16, Dom = 32 }
 }
