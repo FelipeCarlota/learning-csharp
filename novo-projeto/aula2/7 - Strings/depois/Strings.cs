2aa664084f97995f0f264689ef4f82ab18472cfa
FelipeCarlota <felipe.carlota@gmail.com>
2020-12-15 08:19:36 -0400

    atualizando repositório com o conteúdo de Strings

--- novo-projeto/aula2/7 - Strings/depois/Strings.cs
+++ novo-projeto/aula2/7 - Strings/depois/Strings.cs
@@ -10,8 +10,23 @@ namespace certificacao_csharp_roteiro
   {
     public void Executar()
     {
-      // string a = "bom dia";
-      // string b = "b";
+      string a = "bom dia";
+      string b = "b";
+      //Concatenação em C# é feito com o sinal de +
+      b = b + "om dia";
+      Console.WriteLine($"Conferência: a é igual a b?\n {a == b}");
+
+      //Verficando espaço da memória entre as duas variáveis:
+      Console.WriteLine($"Ocupam o mesmo espaço na memória? {(object)a == (object)b}");
+
+      //Imprimindo a letra em uma posição do array de caracteres ou string
+
+      string bomDia = "Bom dia";
+      Console.WriteLine($"A letra na posição 4 é a letra: {bomDia[4]}");
+
+      var caractereQuartaPosicao = bomDia[4];
+      Console.WriteLine($"Tipo do caractere: {caractereQuartaPosicao.GetType()}");
+
     }
   }
 }
