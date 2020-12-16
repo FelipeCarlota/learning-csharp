9abb74d7eaf94c27c77ef5b4e1d5914557ceddde
FelipeCarlota <felipe.carlota@gmail.com>
2020-12-16 17:10:10 -0400

    Atividade sobre métodos sobrecarregados - 16/12/2020 - 17:11

---
+++ novo-projeto/aula4/1_metodos_sobrecarregados/depois/Sobrecargas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+  class Sobrecargas : IAulaItem
+  {
+    public void Executar()
+    {
+      //VOLUME DO CUBO = lado ^ 3;
+      int lado = 3;
+      Console.WriteLine($"O volume do cubo é: {Volume(lado)}");
+
+      //VOLUME DO CILINDRO = altura * PI * raio ^ 2;
+      double raio = 2;
+      int altura = 10;
+      Console.WriteLine($"O volume do cilindro é: {Volume(altura, raio)}");
+
+      //VOLUME DO PRISMA = largura * profundidade * altura
+      long largura = 10;
+      altura = 6;
+      int profundidade = 4;
+      Console.WriteLine($"O volume do prisma é: {Volume(largura, profundidade, altura)}");
+    }
+
+    double Volume(double lado)
+    {
+      return Math.Pow(lado, 3);
+    }
+
+    double Volume(double altura, double raio)
+    {
+      return altura * Math.PI * Math.Pow(raio, 2);
+    }
+
+    double Volume(double largura, double profundidade, double altura)
+    {
+      return largura * profundidade * altura;
+    }
+  }
+}
