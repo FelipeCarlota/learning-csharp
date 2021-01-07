91cdecb08258fb7bc317097a79b4da6a76f88308
FelipeCarlota <you@example.com>
2021-01-07 17:47:35 -0400

    Commit autalizado

---
+++ Atividades/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Atividades
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("===============================================");
+			Console.WriteLine("         Algarismos Romanos por Extenso        ");
+			Console.WriteLine("===============================================");
+
+			string numero;
+
+			Console.WriteLine("Por favor, digite um número romano:");
+			numero = Console.ReadLine();
+
+			//Testando a Condicional if
+			if (numero == "I") {
+				numero = "Um";
+			}
+
+			Console.WriteLine($"O número digitado foi: {numero}");
+		}
+	}
+
+	
+
+}
