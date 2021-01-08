7bf5bc41014c6baaafd1b483c5417e9762477de7
FelipeCarlota <you@example.com>
2021-01-08 01:32:52 -0400

    Deletado algumas linhas

--- Atividades/Program.cs
+++ Atividades/Program.cs
@@ -14,22 +14,26 @@ namespace Atividades
 
 			Console.WriteLine("Por favor, digite um número romano:");
 			numero = Console.ReadLine();
+			
+			// Principais regras 
+			// 1- As letras I, X, C, M só podem ser repetidos três vezes consecutivas. Exemplo: III = 3 e XXX = 30; 
+			if (numero.Contains("IIII") || numero.Contains("XXXX") || numero.Contains("CCCC") || numero.Contains("MMMM")) {
+				Console.WriteLine("Não é um número válido, por favor, tente novamente.");
+			} else {Console.WriteLine($"O número digitado foi: {numero}");}
 
-			// Lista dos números
-			string I = "um";
-		 	string V = "cinco";
-		 	string X = "dez";
-		 	string L = "cinquenta";
-		 	string C = "cem";
-		 	string D = "quinhentos";
-		 	string M = "mil";
+			// 2- No princípio subtrativo, quando duas letras são diferentes e a de menor valor antecede a de maior valor, subtraem-se os seus valores. Exemplo: IV (5 – 1) = 4 e XLV (55 - 10) = 45; 
+			// int I = 1;
+		 	// int V = 5;
+		 	// int X = 10;
+		 	// int L = 50;
+		 	// int C = 100;
+		 	// int D = 500;
+		 	// int M = 1000;
+		
+			// 3- No princípio aditivo, quando duas letras são diferentes e a de maior valor antecede a de menor valor, somam-se os seus valores. Exemplo: VI (5 + 1) = 6 e LIII (50 + 3) = 53; 
 
-			//Testando a Condicional if
-			if (numero == "I") {
-				numero = "Um";
-			}
-
-			Console.WriteLine($"O número digitado foi: {numero}");
+			// 4- Colocando-se um traço sobre uma ou mais letras, seu valor é multiplicado por 1.000;
+			
 		}
 	}
 }
