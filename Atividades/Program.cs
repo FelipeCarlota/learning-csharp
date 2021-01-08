ae4ae3e771782ccaf88ac0ee57b36af4bb6354db
FelipeCarlota <you@example.com>
2021-01-08 07:40:07 -0400

    atualização

--- Atividades/Program.cs
+++ Atividades/Program.cs
@@ -16,20 +16,26 @@ namespace Atividades
 			Console.WriteLine("Por favor, digite um número romano:");
 			numero = Console.ReadLine();
 
+			int[] numeros = {1, 5, 10, 50, 100, 500, 1000};
+			string[] numerosRomanos = {"I", "V", "X", "L", "C", "D", "M"};
+			//encontrar uma forma de relacionar as duas arrays
+
+
 			// 1- As letras I, X, C, M só podem ser repetidos três vezes consecutivas. Exemplo: III = 3 e XXX = 30; 
 			if (numero.Contains("IIII") || numero.Contains("XXXX") || numero.Contains("CCCC") || numero.Contains("MMMM")) {
 				Console.WriteLine("Não é um número válido, por favor, tente novamente.");
 			} else {Console.WriteLine($"O número digitado foi: {numero}");}
 
 			// 2- No princípio subtrativo, quando duas letras são diferentes e a de menor valor antecede a de maior valor, subtraem-se os seus valores. Exemplo: IV (5 – 1) = 4 e XLV (55 - 10) = 45; 
-			int I = 1;
-		 	int V = 5;
-		 	int X = 10;
-		 	int L = 50;
-		 	int C = 100;
-		 	int D = 500;
-		 	int M = 1000;
+			// int I = 1;
+		 	// int V = 5;
+		 	// int X = 10;
+		 	// int L = 50;
+		 	// int C = 100;
+		 	// int D = 500;
+		 	// int M = 1000;
 
+			
 
 			string[] armazenamento;
 			
