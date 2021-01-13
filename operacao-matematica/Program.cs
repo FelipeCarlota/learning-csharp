346561bb45fdad55faae972a47656d3eab151868
FelipeCarlota <you@example.com>
2021-01-13 00:49:32 -0300

    Atualização das atividades

--- operacao-matematica/Program.cs
+++ operacao-matematica/Program.cs
@@ -9,16 +9,16 @@ namespace operacao_matematica
 
             Matematica matematica = new Matematica();
             
-            int soma = matematica.Soma(366,40);
-            Console.WriteLine(soma);
+            // int soma = matematica.Soma(366,40);
+            // Console.WriteLine(soma);
 
-            int subtracao = matematica.Subtrai(370,40);
-            Console.WriteLine(subtracao);
+            // int subtracao = matematica.Subtrai(370,40);
+            // Console.WriteLine(subtracao);
 
-            int multiplicacao = matematica.Multiplica(90,112);
+            int multiplicacao = matematica.Multiplica(5,5);
             Console.WriteLine(multiplicacao);
-            
-            int exponenciacao = matematica.Exponencia(6,6);
+
+            int exponenciacao = matematica.Exponencia(2,3);
             Console.WriteLine(exponenciacao);
 
         }
@@ -51,8 +51,6 @@ namespace operacao_matematica
             public int Multiplica(int multiplicando, int multiplicador) {
                 // var calculo = 0;
                 var multiplicacao = 0;
-                var cont = 0;
-                var armazenaPrimeiroValor = multiplicando;
 
                 for (int i = 0; i < multiplicador; i++) {
                     multiplicacao = multiplicacao + multiplicando;  
@@ -62,14 +60,13 @@ namespace operacao_matematica
             }
             public int Exponencia(int numeroBase, int expoente) {
         
-                var resultado = 0;
-                // var cont = 0;
-                var armazenaPrimeiroValor = numeroBase;
-
-                for (int cont = 0; cont < expoente; cont++) {
-                    for (int i = 0; i < numeroBase; i++) {
-                        resultado = resultado + numeroBase;  
-                    }
+                var resultado = numeroBase;
+                for (int cont = 0; cont < expoente -1; cont++) {
+                    resultado = Multiplica(resultado, numeroBase);
+                    //aux = resultado;
+                    // for (int i = 0; i < numeroBase; i++) {
+                    //  resultado = resultado + numeroBase;  
+                    // }
                 }
 
                 return resultado;
@@ -79,3 +76,24 @@ namespace operacao_matematica
     }
     
 }
+
+
+/*
+expoente 
+
+5 ^ 2 = 25
+
+5 + 5
+  10 + 5
+     15 + 5 = 25
+
+
+3 ^ 7 = 3 . 3 . 3 . 3 . 3 . 3 . 3 = 2187
+
+3 + 3
+  6 + 3
+     9 + 3
+       12 + 3
+
+
+*/
