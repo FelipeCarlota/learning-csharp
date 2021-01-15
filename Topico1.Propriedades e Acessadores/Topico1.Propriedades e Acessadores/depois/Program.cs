47c4c158c0e97df1e055fb890d007c92f9837896
FelipeCarlota <you@example.com>
2021-01-15 02:33:12 -0300

    Primeiro commit aula 3

---
+++ Topico1.Propriedades e Acessadores/Topico1.Propriedades e Acessadores/depois/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cria uma nova instäncia da classe fundionário
+            Funcionario funcionario = new Funcionario();
+
+            //Propriedades
+            // funcionario.salario = 1000;
+            // Console.WriteLine(funcionario.salario);
+
+            // funcionário.salario = -1200;
+            // Console.WriteLine(funcionario.salario);
+            
+            funcionário.Salario = 1200;
+            Console.WriteLine(funcionario.salario);
+        }
+    }   
+
+    class Funcionario
+    {
+        decimal salario;
+
+        public decimal salario {//encapsulacao
+            get
+            {
+                return salario;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("O salário não pode ser negativo");
+                }
+                salario = value;
+            }
+        }
+    }
+}
