fbdb4735884b719ae58c11b6a087f424f6f36518
FelipeCarlota <you@example.com>
2021-01-19 03:37:01 -0400

    atualização aula do alura

---
+++ certificacao_csharp/parte_3/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            funcionario.salario = 1000;
+            Console.WriteLine(funcionario.salario);
+
+            funcionario.salario = -1200;
+            Console.WriteLine(funcionario.salario);
+
+        }
+    }
+
+    class Funcionario
+    {
+        public decimal salario;
+
+        public decimal Salario 
+        {
+            //Encapsulamento do campo salário
+            get
+            {
+                return salario;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("salário não pode ser menor do que 0.");
+                }
+                salario = value;
+            }
+        }
+
+    }
+}
